5b145649d74b92a4a7e4548f88253a969fcb67d2
scosy <amine.bentaleb064@gmail.com>
2022-06-04 09:24:49 +0200

    Changed simple Bot to CommandBot

--- config/initializers/discord_bot.rb
+++ config/initializers/discord_bot.rb
@@ -1,24 +1,24 @@
 module DiscordBot
-    throw "Lacking required secrets!" unless Rails.application.credentials.discord[:token] && Rails.application.credentials.discord[:client_id]
-  
-    @bot = Discordrb::Bot.new(
-      token: Rails.application.credentials.discord[:token],
-      client_id: Rails.application.credentials.discord[:client_id],
-    )
-  
-    puts "This bot's invite URL is #{@bot.invite_url}"
-    puts "Click on it to invite it to your server"
-  
-    @bot.message(with_text: "!ping") do |event|
-      DiscordBot::Commands::Ping.new(event).call
-    end
-  
-    def self.run
-      @bot.run
-    end
-  
-    def self.invite_url
-      @bot.invite_url
-    end
+  throw 'Lacking required secrets!' unless Rails.application.credentials.discord[:token] && Rails.application.credentials.discord[:client_id]
+
+  @bot = Discordrb::Commands::CommandBot.new(
+    token: Rails.application.credentials.discord[:token],
+    client_id: Rails.application.credentials.discord[:client_id],
+    prefix: '!'
+  )
+
+  puts "This bot's invite URL is #{@bot.invite_url}"
+  puts 'Click on it to invite it to your server'
+
+  @bot.command(:ping) do |event|
+    event.respond('pong!')
   end
-  
+
+  def self.run
+    @bot.run
+  end
+
+  def self.invite_url
+    @bot.invite_url
+  end
+end
