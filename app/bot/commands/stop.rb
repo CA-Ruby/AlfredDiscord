5222a477e23902bdae546a0f25d88f3c510315a4
scosy <amine.bentaleb064@gmail.com>
2022-06-07 17:50:54 +0200

    Added command descriptions, and link between start and stop command

--- app/bot/commands/stop.rb
+++ app/bot/commands/stop.rb
@@ -2,9 +2,8 @@ module DiscordBot
   module Stop
     extend Discordrb::Commands::CommandContainer
 
-    command(:stop) do |event|
+    command(:stop, description: "Stoppe la récolte d'informations :teddy_bear:") do |event|
       event.send_message('À bientôt !')
-          
     end
   end
 end
