adcbf2e7717d2db792d172cc5e65a1666f57786e
wilfriedmuffins <wilfried.moffen@live.fr>
2022-06-30 12:12:16 +0200

    :construction: add/update intervals user by command start

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,33 @@
 class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 50 }
+
+  def start_test
+    # Define random new Time
+    tableau_intervalles = []
+    total = 0
+    current_time = Time.now
+    begin
+      nouveau_temps = (3600 + rand(5400))
+      current_time = current_time + nouveau_temps
+      total = total + nouveau_temps
+      tableau_intervalles << current_time.iso8601
+    end until total > 80000
+  
+    intervalles = ""
+    tableau_intervalles.each do |i|
+      intervalles << i
+      intervalles << " "
+    end
+  
+    # Then change user attributes in db
+    current_user = self
+    current_user.update(:flow_test_intervals => intervalles, :flow_testing => true)
+  end
+
+  def stop_test
+    current_user = self
+    current_user.update(:flow_testing => false)
+  end
+  
 end
--- discord/listen_to_start.rb
+++ discord/listen_to_start.rb
@@ -2,7 +2,16 @@ def listen_to_start(bot)
   bot.command(:start, description: "Lance l'algorithme pour récupérer les informations sur ton état de flow") do |event|
     event.send_message("Début de la récolte d'informations :teddy_bear:")
     event.send_message('Tu vas recevoir un formulaire à remplir plusieurs fois dans la journée, pour que je puisse évaluer ton état de flow.')
+    user = User.find(event.user.id)
+    
+    user.start_test
+    p user
     bot.add_await!(Discordrb::Events::MessageEvent) { |message| message.content == '!stop' }
+    
+    user.stop_test
+    p user 
+    
     event.send_message("Fin de la récolte d'informations, merci !")
+   
   end
 end
