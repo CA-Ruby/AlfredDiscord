7dc250b9295c6f0ec08b9fd21c01e5525a07ed3f
scosy <amine.bentaleb064@gmail.com>
2022-07-04 18:18:12 +0200

    :bug: Fixed typo in flow_entries controller

--- app/controllers/flow_entries_controller.rb
+++ app/controllers/flow_entries_controller.rb
@@ -1,7 +1,9 @@
 class FlowEntriesController < ApplicationController
   def show
-    @user = User.find_by(params[:private_id])
+    @user = User.find_by(private_id: params[:id])
+    p "1: #{@user}"
     @flow_entries = FlowEntry.where(user: @user).order(created_at: :desc)
+    p "2: #{@flow_entries}"
     @flow_entries_by_day = FlowEntry.where(user: @user).order(created_at: :desc).group_by { |t| t.created_at.to_date }
   end
 end
