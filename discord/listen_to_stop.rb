129de1a3cefbfd750a972307f240a5e7a824390d
scosy <amine.bentaleb064@gmail.com>
2022-07-04 17:44:30 +0200

    :adhesive_bandage: Added security on stop command to prevent it from being called before start command

--- discord/listen_to_stop.rb
+++ discord/listen_to_stop.rb
@@ -5,6 +5,10 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'responses/upload_res
 def listen_to_stop(bot)
   bot.command(:stop, description: "Stop l'algorithme pour récupérer les informations sur ton état de flow") do |event|
     user = User.find_by(private_id: event.user.id)
+    unless user.flow_testing
+      event.send_message("Veuillez lancer la commande start avant celle-ci s'il-vous-plaît. :teddy_bear:")
+      return nil
+    end
     upload_responses(@form_responses)
     user.stop_test
     event.send_message("Fin de la récolte d'informations, merci !")
