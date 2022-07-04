f1cb3b786cdc76eb1440e21e1c1baf37f96f7556
scosy <amine.bentaleb064@gmail.com>
2022-07-04 17:39:50 +0200

    :bug: Fixed attribute error in flow entry creation

--- discord/responses/upload_responses.rb
+++ discord/responses/upload_responses.rb
@@ -2,7 +2,7 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), '../../lib/assets/get_form_responses'))
 
-@form_responses = @form_responses.reject { |id| User.where(id: id).empty? }
+@form_responses = @form_responses.reject { |id| User.where(private_id: id).empty? }
 
 def upload_responses(responses)
   responses.each do |id, user_hash|
@@ -13,7 +13,7 @@ def upload_responses(responses)
         flow_entry[attribute] = value
       end
       flow_entry.created_at = datetime
-      flow_entry.user_id = id
+      flow_entry.user_id = User.find_by(private_id: id).id
       flow_entry.save unless FlowEntry.find_by(created_at: flow_entry.created_at)
     end
   end
