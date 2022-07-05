00a06b07e76862d380db45bb678c04cc74f03fad
scosy <amine.bentaleb064@gmail.com>
2022-07-05 09:38:20 +0200

    :sparkles: Added link command to allow user to see his results

--- app/controllers/flow_entries_controller.rb
+++ app/controllers/flow_entries_controller.rb
@@ -1,9 +1,7 @@
 class FlowEntriesController < ApplicationController
   def show
     @user = User.find_by(private_id: params[:id])
-    p "1: #{@user}"
     @flow_entries = FlowEntry.where(user: @user).order(created_at: :desc)
-    p "2: #{@flow_entries}"
     @flow_entries_by_day = FlowEntry.where(user: @user).order(created_at: :desc).group_by { |t| t.created_at.to_date }
   end
 end
