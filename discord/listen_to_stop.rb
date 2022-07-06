01ba08d61d175fca69848b0985557d35b1e7f6eb
scosy <amine.bentaleb064@gmail.com>
2022-07-06 08:11:19 +0200

    :art: Replaced 'send_message' by 'message.author.pm' so that the user receives message from the bot in private

--- discord/listen_to_stop.rb
+++ discord/listen_to_stop.rb
@@ -6,11 +6,11 @@ def listen_to_stop(bot)
   bot.command(:stop, description: "Stop l'algorithme pour récupérer les informations sur ton état de flow") do |event|
     user = User.find_by(private_id: event.user.id)
     unless user.flow_testing
-      event.send_message("Veuillez lancer la commande start avant celle-ci s'il-vous-plaît. :teddy_bear:")
+      event.message.author.pm("Veuillez lancer la commande start avant celle-ci s'il-vous-plaît. :teddy_bear:")
       return nil
     end
     upload_responses(@form_responses)
     user.stop_test
-    event.send_message("Fin de la récolte d'informations, merci !")
+    event.message.author.pm("Fin de la récolte d'informations, merci !")
   end
 end
