588c2b9c5abbb513d7b06fec9fe335ff8ded51af
scosy <amine.bentaleb064@gmail.com>
2022-08-03 08:44:55 +0200

    :construction: Made progress with the dropdown, need to pass zones infos to it

--- discord/listen_to_test.rb
+++ discord/listen_to_test.rb
@@ -1,20 +1,35 @@
 require File.expand_path(File.join(File.dirname(__FILE__), 'helpers/set_timezone'))
 
-@regions_list = world_regions.map!.with_index { |w_r, i| "#{i + 1}/ #{w_r}" }
+@regions_list = world_regions
+
 
 def listen_to_test(bot)
   bot.command(:test) do |event|
-    event.respond('Bouton de test') do |_, view|
-      view.row do |r|
-        r.button(label: 'Test!', style: :primary, emoji: 577663465322315786, custom_id: 'test_button:1')
-      end
+    event.channel.send_embed do |embed, view|
+      embed.title = 'Dans quelle région vis-tu ?'
+      embed.description = 'Sélectionne dans la liste ci-dessous'
 
       view.row do |r|
-        r.select_menu(custom_id: 'test_select', placeholder: 'Select me!', max_values: 3) do |s|
-          s.option(label: 'Foo', value: 'foo')
-          s.option(label: 'Bar', value: 'bar')
-          s.option(label: 'Baz', value: 'baz')
-          s.option(label: 'Bazinga', value: 'bazinga')
+        r.select_menu(custom_id: 'test_select', placeholder: 'Select me!', min_values: 1, max_values: 1) do |s|
+          @regions_list.each do |region|
+            s.option(label: region, value: region)
+          end
+        end
+      end
+    end
+  end
+  bot.select_menu(custom_id: 'test_select') do |event|
+    event.respond(content: "You selected: #{event.values.join(', ')}")
+    event.channel.send_embed do |embed, view|
+      embed.title = 'Sur quel fuseau horaire te situes-tu ?'
+      embed.description = 'Sélectionne dans la liste ci-dessous'
+
+      view.row do |r|
+        r.select_menu(custom_id: 'test_select', placeholder: 'Select me!', min_values: 1, max_values: 1) do |s|
+          zones = regions_zones(event.values.join(', '))
+          zones.each do |zone|
+            s.option(label: zone, value: zone)
+          end
         end
       end
     end
