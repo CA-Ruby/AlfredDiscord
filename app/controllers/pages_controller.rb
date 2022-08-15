068d3ff0d1916719580f5c86ede3a539460d11cb
scosy <amine.bentaleb064@gmail.com>
2022-08-15 11:44:29 +0200

    :sparkles: New pages for the user to select his timezone.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
-    def home
-    end
+  require File.expand_path(File.join(File.dirname(__FILE__), '../../discord/helpers/set_timezone'))
+  def home; end
+
+  def select_region
+    @timezones = timezones
+    @regions = world_regions
+  end
+
+  def select_timezone
+    @zones = region_zones(params[:region])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :flow_entries
   get "detail",to: "flow_entries#detail"
-  # Defines the root path route ("/")
+  get "timezone",to: "pages#select_timezone"
+  get "region",to: "pages#select_region"
   root "pages#home"
 end
