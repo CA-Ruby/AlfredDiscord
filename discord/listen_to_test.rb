2480a804866e4ada17519e29760ffd6ff9216e7c
scosy <amine.bentaleb064@gmail.com>
2022-08-17 08:53:51 +0200

    :construction: Need to create command to send the user a link to select his timezone

--- discord/listen_to_test.rb
+++ discord/listen_to_test.rb
@@ -4,39 +4,6 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'helpers/set_timezone
 
 def listen_to_test(bot)
   bot.command(:test) do |event|
-    event.channel.send_embed do |embed, view|
-      embed.title = 'Dans quelle région vis-tu ?'
-      embed.description = 'Sélectionne dans la liste ci-dessous'
-
-      view.row do |r|
-        r.select_menu(custom_id: 'region_select', placeholder: 'Choisis ta région', min_values: 1, max_values: 1) do |s|
-          @regions_list.each do |region|
-            s.option(label: region, value: region)
-          end
-        end
-      end
-    end
-  end
-  bot.select_menu(custom_id: 'region_select') do |event|
-    event.respond(content: "Ta réponse: #{event.values.join(', ')}")
-    event.channel.send_embed do |embed, view|
-      embed.title = 'Sur quel fuseau horaire te situes-tu ?'
-      embed.description = 'Sélectionne dans la liste ci-dessous'
-
-      view.row do |r|
-        r.select_menu(custom_id: 'zone_select', placeholder: 'Choisis ton fuseau horaire !', min_values: 1, max_values: 1) do |s|
-          zones = region_zones(event.values.first)
-          zones.each do |zone|
-            s.option(label: zone, value: zone)
-          end
-
-        end
-      end
-    end
-  end
-  bot.select_menu(custom_id: 'zone_select') do |event|
-    @user_zone = event.values.first
-    p "user = #{@user_zone}"
-    event.respond(content: "well done")
+    ?private_id=454248047187329024
   end
 end
