20cf238228b8a626e71d5bfc5f92792e55ff8342
scosy <amine.bentaleb064@gmail.com>
2022-09-01 16:25:47 +0200

    :speech_balloon: Update link to see results due to change of hosting platform

--- discord/listen_to_timezone.rb
+++ discord/listen_to_timezone.rb
@@ -1,6 +1,6 @@
 def listen_to_timezone(bot)
   bot.command(:timezone, description: "Permet à l'utilisateur de changer son fuseau horaire pour l'affichage de ses résultats") do |event|
     private_id = event.user.id
-    event.message.author.pm("Si tu veux changer le fuseau horaire pour l'affichage de tes résulats, clique sur ce lien: http://alfrediscord.herokuapp.com/region?private_id=#{private_id}")
+    event.message.author.pm("Si tu veux changer le fuseau horaire pour l'affichage de tes résulats, clique sur ce lien: http://alfrediscord.osc-fr1.scalingo.io/region?private_id=#{private_id}")
   end
 end
