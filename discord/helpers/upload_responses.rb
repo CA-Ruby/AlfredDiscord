f49b2bd0e70c582e8afd2f18602ee5c5234cd5e8
scosy <amine.bentaleb064@gmail.com>
2022-09-30 09:16:42 +0200

    :bug: Fixed bug when DB doesn't contain flow entries

--- discord/helpers/upload_responses.rb
+++ discord/helpers/upload_responses.rb
@@ -5,7 +5,9 @@ require File.expand_path(File.join(File.dirname(__FILE__), './get_form_responses
 def upload_responses
   @form_responses = parse_responses(retrieve_responses)
   @form_responses = @form_responses.reject { |id| User.where(private_id: id).empty? }
-  @form_responses.transform_values!{ |hash| hash.reject! { |datetime, d_hash| datetime.to_datetime <= FlowEntry.last.created_at }  }
+  unless FlowEntry.all.size.zero?
+    @form_responses.transform_values!{ |hash| hash.reject! { |datetime, d_hash| datetime.to_datetime <= FlowEntry.last.created_at }  }
+  end
   puts @form_responses
   @form_responses.each do |id, user_hash|
     flow_entry = nil
